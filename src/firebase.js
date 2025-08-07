0431d4557cff3ad07b76c20753e213fdbf6939d8
Noah-eu <david.eder78@gmail.com>
2025-08-07 11:39:23 +0200

    Update firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEUmxYLBn8LExlb2Ei3bUjz6vnEcNHx2Y",
+  authDomain: "putping-dc57e.firebaseapp.com",
+  databaseURL: "https://putping-dc57e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "putping-dc57e",
+  storageBucket: "putping-dc57e.appspot.com", // ✅ Opravený storageBucket
+  messagingSenderId: "244045363394",
+  appId: "1:244045363394:web:64e930bff17a816549635b",
+  measurementId: "G-RLMGM46M6X"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const auth = getAuth(app);
+
+export { db, auth };
